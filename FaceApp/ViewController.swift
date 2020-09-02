056e6b64a2d20a8e35dbd3b87e0453d27f3279f5
DayeonJung <ekdus0227@gmail.com>
2020-09-02 21:54:17 +0900

    Initial Commit

---
+++ FaceApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceApp
+//
+//  Created by Dayeon Jung on 2020/09/02.
+//  Copyright © 2020 Dayeon Jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
